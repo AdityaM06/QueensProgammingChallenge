18193d8eb02a251bd27e5433c707fbb8eda79828
Aditya Manuja <adityamanuja@benunets.com>
2022-08-12 17:08:05 -0400

    Using the draw tab function

--- Client/dashboard_healthcard.py
+++ Client/dashboard_healthcard.py
@@ -61,6 +61,9 @@ def dashboard():
     )
     window.canvas.place(x = 0, y = 0)
 
+    # Adding the tabs
+    functions.draw_tabs()
+
     # Adding "myHealth Dashboard" text to top left
     window.canvas.create_text(
         106.0,
@@ -99,79 +102,6 @@ def dashboard():
         667.0,
         fill="#FFFFFF",
         outline="#FF8888")
-
-
-    # Creating information tab button on top
-    global healthcar_button_image
-    healthcar_button_image = PhotoImage(
-        file='assets/healthcard_button.png')
-    healthcard_button = Button(
-        image=healthcar_button_image,
-        borderwidth=0,
-        highlightthickness=0,
-        command=None,
-        relief="flat"
-    )
-    healthcard_button.place(
-        x=906.2482299804688,
-        y=125.3313980102539,
-        width=266.31109619140625,
-        height=59.18022918701172
-    )
-
-    # Creating vaccine tab button on top
-    global vaccine_button_image
-    vaccine_button_image = PhotoImage(
-        file='assets/vaccine_button.png')
-    vaccine_button = Button(
-        image=vaccine_button_image,
-        borderwidth=0,
-        highlightthickness=0,
-        command=dashboard_vaccine.vaccine_tab,
-        relief="flat"
-    )
-    vaccine_button.place(
-        x=639.9371948242188,
-        y=125.3313980102539,
-        width=266.31103515625,
-        height=59.18022918701172
-    )
-
-    # Creating appointments tab button on top
-    global appointments_button_image
-    appointments_button_image = PhotoImage(
-        file='assets/appointments_button.png')
-    appointments_button = Button(
-        image=appointments_button_image,
-        borderwidth=0,
-        highlightthickness=0,
-        command=lambda: print("button_3 clicked"),
-        relief="flat"
-    )
-    appointments_button.place(
-        x=373.62615966796875,
-        y=125.3313980102539,
-        width=266.31103515625,
-        height=59.18022918701172
-    )
-
-    # Creating records tab button on top
-    global information_button_image
-    information_button_image = PhotoImage(
-        file='assets/information_button.png')
-    information_button = Button(
-        image=information_button_image,
-        borderwidth=0,
-        highlightthickness=0,
-        command=lambda: print("button_4 clicked"),
-        relief="flat"
-    )
-    information_button.place(
-        x=107.31511688232422,
-        y=125.3313980102539,
-        width=266.31103515625,
-        height=59.18022918701172
-    )
 
     # Creating upload button to upload healthcard #1
     global upload_button_image
